eb2f327e539352b5c05a695e776981cb7547739f
Mahamaya-M <71533679+Mahamaya-M@users.noreply.github.com>
2021-08-24 19:06:22 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,99 @@
+song1 = "";
+song2 = "";
+song= "";
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
+scoreLeftWrist = 0;
+scoreRightWrist = 0;
+song_1status=""
+song_2status=""
+
+function preload() {
+    song1 = loadSound("music.mp3");
+    song2 = loadSound("music2.mp3");
+}
+
+function setup() {
+    canvas = createCanvas(500, 400)
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+    console.log('Posenet is intialized');
+}
+
+
+
+
+function gotPoses(results) {
+    if (results.length > 0) {
+        console.log(results);
+        scoreLeftWrist = results[0].pose.keypoints[9].score;
+        scoreRightWrist = results[0].pose.keypoints[10].score;
+        console.log("scoreLeftWrist=" + scoreLeftWrist);
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("leftWristX=" + leftWristX + "leftWristY=" + leftWristY);
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("rightWristX=" + rightWristX + "rightWristY=" + rightWristY);
+
+    }
+}
+
+
+function draw() {
+
+    image(video, 0, 0, 600, 500);
+
+    song_1status= song1.isPlaying();
+    song_2status= song2.isPlaying();
+
+    fill("blue");
+    stroke("blue")
+    if (scoreLeftWrist > 0.2) {
+        circle(leftWristX, leftWristY, 20);
+        song2.stop();
+
+        if(song_1status==false){
+        song=song1;
+        song.play();
+        document.getElementById("song_name").innerHTML="playing "
+        }
+
+    }
+
+   
+    if (scoreRightWrist > 0.2) {
+        circle(rightWristX, rightWristY, 20);
+        song1.stop();
+
+        if(song_2status==false){
+        song=song2;
+        song.play();
+        document.getElementById("song_name").innerHTML="playing "
+        }
+
+    }
+
+
+}
+
+
+function play() {
+    song.play();
+    song.setVolume(1);
+    song.rate(1);
+}
+
+function pause() {
+    song.pause();
+}
